223454e59955d9ccbfb66fdcd3424c8887331b2d
Yerko Arce <yerko.arce.galaz@gmail.com>
2025-04-17 22:26:01 -0400

    First commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Customers() {
+  return (
+    <h1>Customers page</h1>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Invoices () {
+  return (
+    <h1>Invoices page</h1>
+  )
+}
