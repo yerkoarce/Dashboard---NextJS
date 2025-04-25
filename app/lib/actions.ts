038189b08cd6654b7a1db573e43ab21909b19e78
Yerko Arce <yerko.arce.galaz@gmail.com>
2025-04-25 11:30:56 -0400

    Se integra la autenticación con NextAuth, añadiendo la key, los archivos de configuración, el middleware

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import postgres from 'postgres'
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -102,5 +104,24 @@ export async function deleteInvoice(id: string) {
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to delete invoice.');
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
